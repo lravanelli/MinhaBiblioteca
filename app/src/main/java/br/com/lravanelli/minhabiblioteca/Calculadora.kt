8aadd804b8413cb394cab0f5bf050371d3dce893
logonrm <logonrm@fiap.com.br>
2018-03-26 21:01:17 -0300

    Initial commit

---
+++ app/src/main/java/br/com/lravanelli/minhabiblioteca/Calculadora.kt
@@ -0,0 +1,8 @@
+package br.com.lravanelli.minhabiblioteca
+
+class Calculadora() {
+    fun somar(v1: Int, v2: Int): Int {
+        return v1 + v2
+
+    }
+}
---
+++ app/src/main/java/br/com/lravanelli/minhabiblioteca/CustomToast.kt
@@ -0,0 +1,23 @@
+package br.com.lravanelli.minhabiblioteca
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.TextView
+import android.widget.Toast
+
+object CustomToast {
+    fun showToast(context: Context, mensagem: String){
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val view = inflater.inflate(R.layout.custom_toast, null)
+
+        val toast = Toast(context)
+
+        toast.view = view
+        val textView = view.findViewById<TextView>(R.id.textView1)
+        textView.text = mensagem
+
+        toast.duration = Toast.LENGTH_LONG
+        toast.show()
+    }
+}
